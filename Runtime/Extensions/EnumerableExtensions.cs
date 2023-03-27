949c49ac2eda9e7be1fc453434f2324cab524ce0
Byron <byronoud10@gmail.com>
2023-03-27 10:18:14 +0200

    refactor

--- Runtime/Extensions/EnumerableExtensions.cs
+++ Runtime/Extensions/EnumerableExtensions.cs
@@ -10,14 +10,21 @@ namespace Byrone.Utilities.Extensions
 		/// Get a random element from the array
 		/// </summary>
 		/// <param name="array">The array instance</param>
+		/// <param name="rnd">The random instance</param>
 		/// <typeparam name="T">Type of the array elements</typeparam>
 		/// <returns>Random element</returns>
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static T Random<T>(this T[] array, Random rnd = new Random()) =>
+		public static T Random<T>(this T[] array, Random rnd) =>
 			// @todo Optimize 
 			array[rnd.NextInt(0, array.Length)];
 
-		public static void Shuffle<T>(this List<T> list, Random rnd = new Random())
+		/// <summary>
+		/// Shuffle the list contents
+		/// </summary>
+		/// <param name="list">The list instance</param>
+		/// <param name="rnd">The random instance</param>
+		/// <typeparam name="T">Type of the list elements</typeparam>
+		public static void Shuffle<T>(this List<T> list, Random rnd)
 		{
 			var n = list.Count - 1;
 
