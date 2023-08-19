481cf6b047e376a67af056f99b131c27ccfcb337
Byrone <byronoud10@gmail.com>
2023-08-19 22:33:08 +0200

    shuffle refactor

--- Runtime/Extensions/EnumerableExtensions.cs
+++ Runtime/Extensions/EnumerableExtensions.cs
@@ -14,8 +14,7 @@ namespace Byrone.Utilities.Extensions
 		/// <typeparam name="T">Type of the array elements</typeparam>
 		/// <returns>Random element</returns>
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static T Random<T>(this T[] array, Random rnd) =>
-			// @todo Optimize 
+		public static T Random<T>(this T[] array, ref Random rnd) =>
 			array[rnd.NextInt(0, array.Length)];
 
 		/// <summary>
@@ -41,18 +40,24 @@ namespace Byrone.Utilities.Extensions
 		/// <summary>
 		/// Shuffle the array contents
 		/// </summary>
-		/// <param name="list">The array instance</param>
+		/// <param name="array">The array instance</param>
 		/// <param name="rnd">The random instance</param>
+		/// <param name="length">Length of the array</param>
 		/// <typeparam name="T">Type of the list elements</typeparam>
-		public static void Shuffle<T>(this T[] list, ref Random rnd)
+		public static void Shuffle<T>(this T[] array, ref Random rnd, int length = 0)
 		{
-			var n = list.Length - 1;
+			if (length == 0)
+			{
+				length = array.Length;
+			}
+
+			var n = length - 1;
 
 			while (n > 1)
 			{
 				var k = rnd.NextInt(n);
 
-				(list[k], list[n]) = (list[n], list[k]);
+				(array[k], array[n]) = (array[n], array[k]);
 
 				n--;
 			}
